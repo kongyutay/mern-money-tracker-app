36ddca1b5454e0bc70897b8a1722dee0c0faebc5
Tay Kong Yu <kongyutay@gmail.com>
2024-08-10 22:19:09 +0800

    done css

--- src/App.js
+++ src/App.js
@@ -22,7 +22,27 @@ function App() {
             <div className="description">it was time for new tv</div>
           </div>
           <div className="right">
-            <div className="price">$500</div>
+            <div className="price red">-$500</div>
+            <div className="datetime">2022-12-18 15:45</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">Gig job new website</div>
+            <div className="description">it was time for new tv</div>
+          </div>
+          <div className="right">
+            <div className="price green">+$400</div>
+            <div className="datetime">2022-12-18 15:45</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">Iphone</div>
+            <div className="description">it was time for new tv</div>
+          </div>
+          <div className="right">
+            <div className="price red">-$900</div>
             <div className="datetime">2022-12-18 15:45</div>
           </div>
         </div>
