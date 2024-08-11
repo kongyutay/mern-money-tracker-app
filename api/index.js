3da134d18731476d9755bb0835531ec86665f100
Tay Kong Yu <kongyutay@gmail.com>
2024-08-11 22:11:27 +0800

    use mongoose to create schema and model but havent build a database on mongo atlas

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.get('/api/test', (req, res) => {
     res.json('test ok')
 })
 
 app.post('/api/transaction', (req, res) => {
+    const {name, description, datetime} = res.body;
+    
     res.json(req.body)
 })
 
