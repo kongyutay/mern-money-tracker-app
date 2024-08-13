3a47435b025a6d3bb5aebe0b9bbe9a5f06d09713
Tay Kong Yu <kongyutay@gmail.com>
2024-08-13 21:40:53 +0800

    have to change dns to 8.8.8.8 due to some cellphone hotspot not support srv dns record

--- api/index.js
+++ api/index.js
@@ -12,8 +12,8 @@ app.get('/api/test', (req, res) => {
 })
 
 app.post('/api/transaction', async (req, res) => {
-    //console.log(process.env.MONGO_URL)
-    await mongoose.connect(process.env.MONGO_URL, { ssl: false })
+    console.log("mongourl", process.env.MONGO_URL)
+    await mongoose.connect(process.env.MONGO_URL)
     const {name, price, description, datetime} = req.body;
     const transaction = await Transaction.create({
         name, 
