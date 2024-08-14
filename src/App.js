adb6ea8da057d611017ad90401ef60b9d6036381
Tay Kong Yu <kongyutay@gmail.com>
2024-08-14 21:53:32 +0800

    render the transactions list

--- src/App.js
+++ src/App.js
@@ -1,73 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [name, setName] = useState('');
-  const [datetime, setDatetime] = useState('');
-  const [description, setDescription] = useState('')
+  const [name, setName] = useState("");
+  const [datetime, setDatetime] = useState("");
+  const [description, setDescription] = useState("");
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+    getTransactions().then(setTransactions);
+  }, []);
+
+  async function getTransactions() {
+    const url = process.env.REACT_APP_API_URL + "/transactions";
+    const response = await fetch(url);
+    return await response.json();
+  }
+
   function addNewTransaction(ev) {
-    ev.preventDefault()
-    const url = process.env.REACT_APP_API_URL + '/transaction';
-    const price = name.split(' ')[0];
+    ev.preventDefault();
+    const url = process.env.REACT_APP_API_URL + "/transaction";
+    const price = name.split(" ")[0];
     fetch(url, {
-      method: 'POST',
-      headers: {'Content-type':'application/json' },
-      body: JSON.stringify({price, name: name.substring(price.length + 1), description, datetime})
-    }).then(res => {
-      res.json().then(json=> {
-        setName('')
-        setDatetime('')
-        setDescription('')
-        console.log('result', json)
-      })
-    })
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        price,
+        name: name.substring(price.length + 1),
+        description,
+        datetime,
+      }),
+    }).then((res) => {
+      res.json().then((json) => {
+        setName("");
+        setDatetime("");
+        setDescription("");
+        console.log("result", json);
+      });
+    });
     // fetch()
   }
   return (
     <main>
-      <h1>$400 <span>.00</span></h1>
+      <h1>
+        $400 <span>.00</span>
+      </h1>
       <form onSubmit={addNewTransaction}>
-        <div className='basic'>
-          <input type="text" value={name} onChange={ev => setName(ev.target.value)} placeholder={'+200 new samsung tv'}/>
-          <input type="datetime-local" value={datetime} onChange={ev => setDatetime(ev.target.value)}/>
+        <div className="basic">
+          <input
+            type="text"
+            value={name}
+            onChange={(ev) => setName(ev.target.value)}
+            placeholder={"+200 new samsung tv"}
+          />
+          <input
+            type="datetime-local"
+            value={datetime}
+            onChange={(ev) => setDatetime(ev.target.value)}
+          />
         </div>
-        <div className='description'>
-          <input type="text" value={description} placeholder={'description'} onChange={ev => setDescription(ev.target.value)}/>
+        <div className="description">
+          <input
+            type="text"
+            value={description}
+            placeholder={"description"}
+            onChange={(ev) => setDescription(ev.target.value)}
+          />
         </div>
-        <button type='submit'>Add new transaction</button>
+        <button type="submit">Add new transaction</button>
       </form>
       <div className="transactions">
-        <div className="transaction">
-          <div className="left">
-            <div className="name">New Samsung TV</div>
-            <div className="description">it was time for new tv</div>
-          </div>
-          <div className="right">
-            <div className="price red">-$500</div>
-            <div className="datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Gig job new website</div>
-            <div className="description">it was time for new tv</div>
-          </div>
-          <div className="right">
-            <div className="price green">+$400</div>
-            <div className="datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Iphone</div>
-            <div className="description">it was time for new tv</div>
-          </div>
-          <div className="right">
-            <div className="price red">-$900</div>
-            <div className="datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
+        {transactions.length > 0 &&
+          transactions.map((transaction) => (
+            <div className="transaction">
+              <div className="left">
+                <div className="name">{transaction.name}</div>
+                <div className="description">{transaaction.description}</div>
+              </div>
+              <div className="right">
+                <div
+                  className={
+                    "price" + (transaction.price < 0 ? "red" : "green")
+                  }
+                >
+                  {transaction.price}
+                </div>
+                <div className="datetime">{transaction.datetime}</div>
+              </div>
+            </div>
+          ))}
       </div>
     </main>
   );
